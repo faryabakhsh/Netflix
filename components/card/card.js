43e4edb843be50535effd832adee9a23553285f6
Farya Bakhsh <faryabakhsh@gmail.com>
2022-10-10 11:35:21 +0500

    error handling

--- components/card/card.js
+++ components/card/card.js
@@ -1,7 +1,19 @@
 import Image from "next/image";
+import styles from "./card.module.css";
+import { useState } from "react";
 
 const Card = (props) => {
-  const { imgUrl, size } = props;
+  const {
+    imgUrl = "https://images.unsplash.com/photo-1542423348-821c6bb30fe6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
+    size = medium,
+  } = props;
+  const [imgSrc, setImgSrc] = useState(imgUrl);
+  const onhandleError = () => {
+    console.log(" hi error");
+    setImgSrc(
+      "https://images.unsplash.com/photo-1542423348-821c6bb30fe6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80"
+    );
+  };
   const classMap = {
     large: styles.lgItem,
     medium: styles.mdItem,
@@ -10,8 +22,15 @@ const Card = (props) => {
 
   return (
     <div className={styles.container}>
+      <Image src={imgUrl} alt="image" width="300px" height="300px" />
       <div className={classMap[size]}>
-        <Image src={imgUrl} alt="image" layout="fill" />
+        <Image
+          src={imgSrc}
+          alt="image"
+          layout="fill"
+          className={styles.cardImg}
+          onError={onhandleError}
+        />
       </div>
     </div>
   );
