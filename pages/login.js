e060a78d0fce984210c3ccb897c9019fc5b04898
Farya Bakhsh <faryabakhsh@gmail.com>
2022-11-10 23:51:32 +0500

    build sign in page

---
+++ pages/login.js
@@ -0,0 +1,50 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/login.module.css";
+
+const Login = () => {
+  const handleLoginWithEmail = (e) => {
+    console.log("hi button");
+    e.preventDefault();
+  };
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Netflix SignIn</title>
+      </Head>
+
+      <header className={styles.header}>
+        <div className={styles.headerWrapper}>
+          <Link className={styles.logoLink} href="/">
+            <div className={styles.logoWrapper}>
+              <Image
+                src="/static/netflix.svg"
+                alt="Netflix logo"
+                width="128px"
+                height="34px"
+              />
+            </div>
+          </Link>
+        </div>
+      </header>
+
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signinHeader}>Sign In</h1>
+          <input
+            type="text"
+            placeholder="Emial address"
+            className={styles.emailInput}
+          />
+
+          <button className={styles.loginBtn} onClick={handleLoginWithEmail}>
+            Sign In
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Login;
