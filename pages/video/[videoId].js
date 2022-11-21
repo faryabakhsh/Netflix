e9697ad9b92f280983be96cba6f56dcbbf8edc7a
Farya Bakhsh <faryabakhsh@gmail.com>
2022-11-21 23:42:23 +0500

    modal component structure implementation

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,11 +1,24 @@
 import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/video.module.css";
+import clsx from "classnames";
 
 Modal.setAppElement("#__next");
 
 const Video = () => {
   const router = useRouter();
+
+  const video = {
+    title: "Hi cute dog",
+    publishTime: "1990-01-01",
+    description:
+      "A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger?",
+    channelTitle: "Paramount Pictures",
+    viewCount: 10000,
+  };
+
+  const { title, publishTime, description, channelTitle, viewCount } = video;
+
   return (
     <div className={styles.container}>
       <Modal
@@ -16,6 +29,7 @@ const Video = () => {
         overlayClassName={styles.overlay}
       >
         <iframe
+          className={styles.videoPlayer}
           id="ytplayer"
           type="text/html"
           width="100%"
@@ -23,6 +37,25 @@ const Video = () => {
           src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
           frameBorder="0"
         ></iframe>
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   );
