731a7cb14919582b1bde9072a6b1982c2f1ff145
Farya Bakhsh <faryabakhsh@gmail.com>
2022-11-25 22:09:28 +0500

    authentication initiated

---
+++ pages/api/login.js
@@ -0,0 +1,14 @@
+export default async function login(req, res) {
+  if (req.method === "POST") {
+    try {
+      res.send({ done: true });
+    } catch (error) {
+      console
+        .error("something went wrong logging in", error)
+        .res.status(500)
+        .send({ done: false }); // send  function is going to act as a return so it should be after console. send should be after console so that we can see the error before it returns anything
+    }
+  } else {
+    res.send({ done: false });
+  }
+}
