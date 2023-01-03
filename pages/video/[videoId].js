6fdeee5fefce4e8ccb12ee568abd246fa6a0be14
Farya Bakhsh <faryabakhsh@gmail.com>
2023-01-03 10:06:01 +0500

    set like dislike buttons

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -108,6 +108,8 @@ import NavBar from "../../components/nav/navbar";
 import clsx from "classnames";
 
 import { getYoutubeVideoById } from "../../lib/videos";
+import Like from "../../components/icons/like-icon";
+import DisLike from "../../components/icons/dislike-icon";
 
 Modal.setAppElement("#__next");
 
@@ -136,8 +138,8 @@ const Video = ({ video }) => {
   const router = useRouter();
   const videoId = router.query.videoId;
 
-  // const [toggleLike, setToggleLike] = useState(false);
-  // const [toggleDisLike, setToggleDisLike] = useState(false);
+  const [toggleLike, setToggleLike] = useState(false);
+  const [toggleDisLike, setToggleDisLike] = useState(false);
 
   const {
     title,
@@ -146,6 +148,19 @@ const Video = ({ video }) => {
     channelTitle,
     statistics: { viewCount } = { viewCount: 0 },
   } = video;
+
+  const handleToggleLike = () => {
+    console.log("like");
+
+    setToggleLike(!toggleLike);
+    setToggleDisLike(toggleLike);
+  };
+
+  const handleToggleDisLike = () => {
+    console.log("dialike");
+    setToggleDisLike(!toggleDisLike);
+    setToggleLike(toggleDisLike);
+  };
 
   // useEffect(() => {
   //   const handleLikeDislikeService = async () => {
@@ -216,6 +231,21 @@ const Video = ({ video }) => {
           src={`https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
           frameBorder="0"
         ></iframe>
+        <div className={styles.likeDislikeBtnWrapper}>
+          <div className={styles.likeBtnWrapper}>
+            <button onClick={handleToggleLike}>
+              <div className={styles.btnWrapper}>
+                <Like selected={toggleLike} />
+              </div>
+            </button>
+          </div>
+          <button onClick={handleToggleDisLike}>
+            <div className={styles.btnWrapper}>
+              <DisLike selected={toggleDisLike} />
+            </div>
+          </button>
+        </div>
+
         {/* 
         <div className={styles.likeDislikeBtnWrapper}>
           <div className={styles.likeBtnWrapper}>
