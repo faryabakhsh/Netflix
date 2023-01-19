a1f59832ca0a91b4473929ffba019d36b1cebe0c
Farya Bakhsh <faryabakhsh@gmail.com>
2023-01-20 00:19:52 +0500

    Use Query String For Video ID

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -3,20 +3,19 @@ import { findVideoIdByUser } from "../../lib/db/hasura";
 
 export default async function stats(req, resp) {
   if (req.method === "POST") {
-    console.log({ cookies: req.cookies });
     try {
       const token = req.cookies.token;
       if (!token) {
         resp.status(403).send({});
       } else {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log({ decoded });
+        const videoId = req.query.videoId;
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
-        const userId = "did:ethr:0x4f0Bfc842885E2775Ef77109222E9EBAce6aaA04";
-        const videoId = "4zH5iYM4wJo";
+        const userId = decodedToken.issuer;
+        // const videoId = "4zH5iYM4wJo";
         const findVideoId = await findVideoIdByUser(token, userId, videoId);
-        console.log({ findVideoId });
-        resp.send({ msg: "it works", decoded, findVideoId });
+
+        resp.send({ msg: "it works", decodedToken, findVideoId });
       }
     } catch (error) {
       console.error("Error occurred /stats", error);
